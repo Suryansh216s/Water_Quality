325153ffdbfa5829b48fe6e96204f2a76a487f20
Suryansh216s <suryansh216@gmail.com>
2025-05-12 22:12:34 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -17,8 +17,8 @@ st.set_page_config(page_title="Water Quality Predictor", layout="centered")
 st.markdown("""
     <style>
     .block-container {
-        padding-top: 0rem;
-        margin-top: 0rem;
+        padding-top: 2rem;
+        margin-top: 2rem;
     }
     </style>
     """, unsafe_allow_html=True)
